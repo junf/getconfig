44603c0af0ca6ed14edf89655fe3b753c630f8fa
Jun Funada <jun2@alt-area.flop.jp>
2013-02-11 10:43:31 +0900

    Change display by TextView to ListView
    I think list is more appropriate than text.

--- src/com/example/getconfig/MyActivity.java
+++ src/com/example/getconfig/MyActivity.java
@@ -3,10 +3,15 @@ package com.example.getconfig;
 import android.app.Activity;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MyActivity extends Activity {
 	/**
@@ -21,24 +26,31 @@ public class MyActivity extends Activity {
 	}
 
 	private void displayConfig() {
+		ArrayList<Map<String,String>> list = new ArrayList<Map<String,String>>();
 		Configuration config = getResources().getConfiguration();
 		Field[] fields = config.getClass().getDeclaredFields();
-		StringBuilder str = new StringBuilder();
 		try {
 			for(Field field: fields) {
 				final int modifier = field.getModifiers();
 				if (!Modifier.isStatic(modifier)) {
-					str.append(field.getName());
-					str.append(":");
-					str.append(field.get(config));
-					str.append("\n");
+					Map<String,String> map = new HashMap<String,String>();
+					map.put("item_name", field.getName());
+					map.put("item_value", field.get(config).toString());
+					list.add(map);
 				}
 			}
 		} catch (IllegalAccessException e) {
 			//I don' think if throwing this exception is appropriate.
 			throw new RuntimeException(e);
 		}
-		TextView tv = (TextView)findViewById(R.id.text_config);
-		tv.setText(str.toString());
+
+		SimpleAdapter adapter = new SimpleAdapter(
+				this,
+				list,
+				R.layout.config_row,
+				new String[]{"item_name","item_value"},
+				new int[]{R.id.item_name, R.id.item_value});
+		ListView view = (ListView)findViewById(R.id.list_config);
+		view.setAdapter(adapter);
 	}
 }
